70b16a6fcf00844f3bcd367cdfb706ba39ba528a
jbescos <jorgebescos@gmail.com>
2021-08-16 19:18:24 +0200

    Implemented panic sell
    Signed-off-by: jbescos <jorgebescos@gmail.com>

--- cloud/packages/cloud-bot/src/main/java/com/jbescos/cloudbot/BotFunction.java
+++ cloud/packages/cloud-bot/src/main/java/com/jbescos/cloudbot/BotFunction.java
@@ -1,5 +1,6 @@
 package com.jbescos.cloudbot;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -13,10 +14,12 @@ import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
 import com.google.cloud.storage.StorageOptions;
 import com.jbescos.common.BinanceAPI;
-import com.jbescos.common.BucketStorage;
 import com.jbescos.common.Broker;
 import com.jbescos.common.Broker.Action;
+import com.jbescos.common.BucketStorage;
 import com.jbescos.common.CloudProperties;
+import com.jbescos.common.CsvRow;
+import com.jbescos.common.PanicBroker;
 import com.jbescos.common.SecureBinanceAPI;
 import com.jbescos.common.Utils;
 
@@ -36,11 +39,24 @@ public class BotFunction implements HttpFunction {
 		SecureBinanceAPI api = SecureBinanceAPI.create(client, storage);
 		String side = Utils.getParam(SIDE_PARAM, null, request.getQueryParameters());
 		if (side != null) {
-			LOGGER.info("Actively invoked to sell or buy");
-			String symbol = Utils.getParam(SYMBOL_PARAM, null, request.getQueryParameters());
-			String quantity = Utils.getParam(QUANTITY_PARAM, null, request.getQueryParameters());
-			Map<String, String> apiResponse = api.orderSymbol(symbol, Action.valueOf(side), quantity);
-			response.getWriter().write(apiResponse.toString());
+			Action action = Action.valueOf(side) ;
+			LOGGER.info("Actively invoked " + side);
+			if (Action.SELL_PANIC == action) {
+				Date now = new Date();
+				List<Broker> stats = new BinanceAPI(client).price().stream()
+						.filter(price -> CloudProperties.BOT_WHITE_LIST_SYMBOLS.contains(price.getSymbol()))
+						.map(price -> new CsvRow(now, price.getSymbol(), price.getPrice()))
+						.map(row -> new PanicBroker(row.getSymbol(), row, 0))
+						.collect(Collectors.toList());
+				BotBinance bot = new BotBinance(api);
+				bot.execute(stats);
+				response.getWriter().write(stats.toString());
+			} else {
+				String symbol = Utils.getParam(SYMBOL_PARAM, null, request.getQueryParameters());
+				String quantity = Utils.getParam(QUANTITY_PARAM, null, request.getQueryParameters());
+				Map<String, String> apiResponse = api.orderSymbol(symbol, Action.valueOf(side), quantity);
+				response.getWriter().write(apiResponse.toString());
+			}
 		} else {
 			List<Broker> stats = BotUtils.loadStatistics(client, true).stream()
 					.filter(stat -> stat.getAction() != Action.NOTHING).collect(Collectors.toList());
