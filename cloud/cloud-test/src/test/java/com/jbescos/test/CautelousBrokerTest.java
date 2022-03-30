d86c195579b5e946f479a8380512c1f702f1bc62
jbescos <jorgebescos@gmail.com>
2022-03-30 21:26:02 +0200

    Fix bot.lowest.allowed.profit.sell
    Signed-off-by: jbescos <jorgebescos@gmail.com>

---
+++ cloud/cloud-test/src/test/java/com/jbescos/test/CautelousBrokerTest.java
@@ -0,0 +1,34 @@
+package com.jbescos.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.jbescos.common.Broker.Action;
+import com.jbescos.common.CautelousBroker;
+import com.jbescos.common.CloudProperties;
+import com.jbescos.common.CsvRow;
+import com.jbescos.common.CsvTransactionRow;
+import com.jbescos.common.CsvUtil;
+import com.jbescos.common.Utils;
+
+public class CautelousBrokerTest {
+
+	@Test
+	public void gensSell() throws IOException {
+		CloudProperties cloudProperties = new CloudProperties("kucoin", null);
+		try (BufferedReader csvReader = new BufferedReader(new InputStreamReader(CautelousBrokerTest.class.getResourceAsStream("/broker/GENSUSDT/GENSUSDT.csv")));
+				BufferedReader txReader = new BufferedReader(new InputStreamReader(CautelousBrokerTest.class.getResourceAsStream("/broker/GENSUSDT/transactions.csv")));) {
+			List<CsvRow> values = CsvUtil.readCsvRows(true, ",", csvReader, Collections.emptyList());
+			List<CsvTransactionRow> previousTransactions = CsvUtil.readCsvTransactionRows(true, ",", txReader);
+			CautelousBroker broker = new CautelousBroker(cloudProperties, "GENSUSDT", values, Utils.minSellProfitable(previousTransactions), false);
+			assertEquals(Action.SELL, broker.getAction());
+		}
+	}
+}
