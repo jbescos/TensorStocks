1d23ccb8b9b50380574d5eee3d57b56c4c436033
jbescos <jorgebescos@gmail.com>
2023-02-02 06:56:44 +0100

    Add logger in news
    Signed-off-by: jbescos <jorgebescos@gmail.com>

--- cloud/packages/cloud-common/src/main/java/com/jbescos/common/NewsUtils.java
+++ cloud/packages/cloud-common/src/main/java/com/jbescos/common/NewsUtils.java
@@ -14,23 +14,26 @@ import com.jbescos.exchange.Utils;
 
 public class NewsUtils {
 
-	private static final Logger LOGGER = Logger.getLogger(NewsUtils.class.getName());
-	
+    private static final Logger LOGGER = Logger.getLogger(NewsUtils.class.getName());
+
     public static void news(long millis, PublicAPI publicAPI, CloudProperties cloudProperties, Client client) {
-    	try (TelegramBot telegram = new TelegramBot(cloudProperties, client)) {
-	        long minutes30Back = millis - (30 * 60 * 1000);
-	        Date rounded = Utils.dateRoundedTo10Min(new Date(minutes30Back));
-	        for (Exchange exchange : Exchange.values()) {
-	        	try {
-		        	List<News> news = exchange.news(publicAPI, rounded.getTime());
-		        	for (News n : news) {
-		        		telegram.exception(n.toString(), null);
-		        	}
-	        	} catch (Exception e) {
-	        		LOGGER.log(Level.SEVERE, "Cannot obtain news from " + exchange.name(), e);
-	        	}
-	        }
+        try (TelegramBot telegram = new TelegramBot(cloudProperties, client)) {
+            long minutes30Back = millis - (30 * 60 * 1000);
+            Date rounded = Utils.dateRoundedTo10Min(new Date(minutes30Back));
+            for (Exchange exchange : Exchange.values()) {
+                try {
+                    List<News> news = exchange.news(publicAPI, rounded.getTime());
+                    if (!news.isEmpty()) {
+                        LOGGER.info("Notifying " + news.size() + " news for " + exchange.name());
+                    }
+                    for (News n : news) {
+                        telegram.exception(n.toString(), null);
+                    }
+                } catch (Exception e) {
+                    LOGGER.log(Level.SEVERE, "Cannot obtain news from " + exchange.name(), e);
+                }
+            }
         }
     }
-    
+
 }
