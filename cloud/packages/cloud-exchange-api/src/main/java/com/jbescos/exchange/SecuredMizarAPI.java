b58c998da196dff31f977a297236638c8aa9b612
jbescos <jorgebescos@gmail.com>
2023-02-26 10:56:17 +0100

    Update Mizar URL
    Signed-off-by: jbescos <jorgebescos@gmail.com>

--- cloud/packages/cloud-exchange-api/src/main/java/com/jbescos/exchange/SecuredMizarAPI.java
+++ cloud/packages/cloud-exchange-api/src/main/java/com/jbescos/exchange/SecuredMizarAPI.java
@@ -27,7 +27,7 @@ public class SecuredMizarAPI implements SecuredAPI {
     private static final Logger LOGGER = Logger.getLogger(SecuredMizarAPI.class.getName());
     private static final String DEFAULT_WALLET_CONTENT = "1000000000";
     private final Map<String, String> wallet = new HashMap<>();
-    private static final String URL = "https://api.mizar.ai/api/v1";
+    private static final String URL = "https://api.mizar.com/api/v1";
     private static final String HEADER_API = "mizar-api-key";
     private final Client client;
     private final PropertiesMizar cloudProperties;
