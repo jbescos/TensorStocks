4925dfd2ff6d8aa19bf083dceda042944fe2d4f6
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2023-04-14 08:34:22 +0200

    REmove overwrite properties
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- cloud/packages/local-bot/script/run.sh
+++ cloud/packages/local-bot/script/run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-CURRENT_DIR=`pwd`
-PROPERTIES_SRC="$HOME/bot/crypto-properties/cloud.properties"
-GIT_SRC="$HOME/workspace/TensorStocks"
-cd $GIT_SRC
-git pull --force
-cp $GIT_SRC/cloud/cloud-test/src/main/resources/cloud.properties $PROPERTIES_SRC
-cd $CURRENT_DIR
-
 # Run only Binance
 timeout 280s java -jar $HOME/workspace/TensorStocks/cloud/packages/local-bot/target/local-bot-1.0.jar BINANCE
