0ccd32661592e62b08fb77d7e16e30db9dea970b
jbescos <jorgebescos@gmail.com>
2024-08-06 18:48:30 +0200

    Timezone change and JDK21
    Signed-off-by: jbescos <jorgebescos@gmail.com>

--- cloud/redeploy.sh
+++ cloud/redeploy.sh
@@ -17,10 +17,10 @@ uploadZips() {
   gsutil cp packages/cloud-bot/target/cloud-bot-1.0.zip ${SOURCES}
 }
 createFunctions() {
-  gcloud functions deploy function-crypto-storage --trigger-http --runtime=java11 --allow-unauthenticated --memory=512MB --source=${SOURCES}cloud-bot-1.0.zip --stage-bucket=${BUCKET} --timeout=280s --entry-point=com.jbescos.cloudstorage.StorageFunction --project=${PROJECT}
-  gcloud functions deploy bot --trigger-topic=storage --runtime=java11 --memory=512MB --source=${SOURCES}cloud-bot-1.0.zip --stage-bucket=${BUCKET} --timeout=280s --entry-point=com.jbescos.cloudbot.BotSubscriber --project=${PROJECT}
-  gcloud functions deploy chart --trigger-http --runtime=java11 --allow-unauthenticated --memory=512MB --source=${SOURCES}cloud-bot-1.0.zip --stage-bucket=${BUCKET} --timeout=280s --entry-point=com.jbescos.cloudchart.ChartFunction --project=${PROJECT}
-  gcloud functions deploy chartFileListener --trigger-event=google.storage.object.finalize --trigger-resource=crypto-for-training --runtime=java11 --memory=512MB --source=${SOURCES}cloud-bot-1.0.zip --stage-bucket=${BUCKET} --timeout=280s --entry-point=com.jbescos.cloudchart.ChartFileListener --project=${PROJECT}
+  gcloud functions deploy function-crypto-storage --region=europe-west1 --trigger-http --runtime=java21 --allow-unauthenticated --memory=512MB --source=${SOURCES}cloud-bot-1.0.zip --stage-bucket=${BUCKET} --timeout=280s --entry-point=com.jbescos.cloudstorage.StorageFunction --project=${PROJECT}
+  gcloud functions deploy bot --region=europe-west1 --trigger-topic=storage --runtime=java21 --memory=512MB --source=${SOURCES}cloud-bot-1.0.zip --stage-bucket=${BUCKET} --timeout=280s --entry-point=com.jbescos.cloudbot.BotSubscriber --project=${PROJECT}
+  gcloud functions deploy chart --region=europe-west1 --trigger-http --runtime=java21 --allow-unauthenticated --memory=512MB --source=${SOURCES}cloud-bot-1.0.zip --stage-bucket=${BUCKET} --timeout=280s --entry-point=com.jbescos.cloudchart.ChartFunction --project=${PROJECT}
+  gcloud functions deploy chartFileListener --region=europe-west1 --trigger-event=google.storage.object.finalize --trigger-resource=crypto-for-training --runtime=java21 --memory=512MB --source=${SOURCES}cloud-bot-1.0.zip --stage-bucket=${BUCKET} --timeout=280s --entry-point=com.jbescos.cloudchart.ChartFileListener --project=${PROJECT}
 }
 
 if [ $# -lt 1 ]; then
